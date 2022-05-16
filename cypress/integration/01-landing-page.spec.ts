481eb89a7c7cd906b16a96273f6109e65ec61d76
Jeffry Gonzalez <jeff@hypertheory.com>
2022-05-16 14:58:17 -0400

    landing page done for now

--- cypress/integration/01-landing-page.spec.ts
+++ cypress/integration/01-landing-page.spec.ts
@@ -10,4 +10,33 @@ describe('The Landing Page', () => {
   it('does not have the logout button', () => {
     cy.get('[data-auth-logout-button]').should('not.exist');
   });
+  it('displays the home component', () => {
+    cy.get('[data-home-component]').should('exist');
+  });
+
+  describe('navigation', () => {
+
+    beforeEach(() => {
+      cy.visit('/');
+    })
+
+  describe('course list link', () => {
+    it('has the course list link', () => {
+      cy.get('[data-nav-course-list]').should('exist');
+
+    });
+    it('shows the course list component', () => {
+      cy.get('[data-nav-course-list]').click();
+      cy.get('[data-courses-component]').should('exist');
+    })
+
+  });
+
+  describe('the hypertheory training link', () => {
+
+    it('has the link', () => {})
+
+    it('shows the home component', () => {})
+  });
+});
 });
